cdd6919cbc9f79c70627ba9b64939971caa66233
Hishita Bora <80160058+Chinu1207Hishita@users.noreply.github.com>
2021-03-18 22:21:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+var first_name = "Hishita";
+var last_name = "Bora";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 30;
+var b = 29;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiple = a * b ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
